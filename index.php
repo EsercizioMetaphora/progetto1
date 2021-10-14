7e17f781d34e8db8e959ff0189ec12a0039bbf14
Danixx91 <daniele_1991@hotmail.com>
2021-10-14 18:05:22 +0200

    db php homepage

--- index.php
+++ index.php
@@ -30,10 +30,10 @@
             <?php foreach (ARTICLES as $article) : ?>
                 <article class="col-6 col-lg-3">
                     <div class="card border-0">
-                        <img src="<?php echo $article['immagine'] ?>" class="card-img-top" alt="<?php echo $article['alt'] ?>">
+                        <img src="\images\articles\<?php echo $article['img'] ?>" class="card-img-top" alt="<?php echo $article['alt_text'] ?>">
                         <div class="card-body px-0">
-                            <h5 class="card-title"><?php echo $article['titolo'] ?></h5>
-                            <p class="card-text"><?php echo $article['testo'] ?></p>
+                            <h5 class="card-title"><?php echo $article['title'] ?></h5>
+                            <p class="card-text"><?php echo $article['textarticles'] ?></p>
                             <div class="d-flex">
                                 <a href="/details.php" class="flex-fill btn btn-primary">Info</a>
                                 <a href="#" class="btn btn-primary ms-3"><i class="bi bi-bag-plus-fill"></i></a>
--- partials/_website_data.php
+++ partials/_website_data.php
@@ -31,29 +31,44 @@ $menu = [
 
 define('WEBSITE_MENU', $menu);
 
-$carousel_home = [
+
+$sth = $pdo->prepare("SELECT * FROM carousel WHERE attivo=1 ORDER BY ordine");
+$sth->execute();
+
+$carousel_home = $sth->fetchAll(\PDO::FETCH_ASSOC);
+
+
+/*$carousel_home = [
     ['src' => 'images/static_assets/IMG_4139.JPG', 'alt' => 'Immagine 1'],
     ['src' => 'images/static_assets/IMG_4140.JPG', 'alt' => 'Immagine 2'],
     ['src' => 'images/static_assets/IMG_4155.JPG', 'alt' => 'Immagine 3'],
     ['src' => 'images/static_assets/IMG_4170.JPG', 'alt' => 'Immagine 4']
-];
+];*/
 
+$sth = $pdo->prepare("SELECT * FROM news");
+$sth->execute();
 
-$news = [
-    ['immagine' => '/images/immagini/IMG_4155a.jpg', 'alt' => 'Immagine1', 'titolo' => 'Titolo 1', 'testo' => 'Testo 1 eccetera', 'data' => 'Data1'],
-    ['immagine' => '/images/immagini/IMG_4155.jpg', 'alt' => 'Immagine2', 'titolo' => 'Titolo 2', 'testo' => 'Testo 2 eccetera', 'data' => 'Data2'],
-    ['immagine' => '/images/immagini/IMG_4155a.jpg', 'alt' => 'Immagine1', 'titolo' => 'Titolo 3', 'testo' => 'Testo 3 eccetera', 'data' => 'Data3'],
-    ['immagine' => '/images/immagini/IMG_4155a.jpg', 'alt' => 'Immagine1', 'titolo' => 'Titolo 4', 'testo' => 'Testo 4 eccetera', 'data' => 'Data4']
-];
+$news = $sth->fetchAll(\PDO::FETCH_ASSOC);
+
+/*$news = [
+    ['immagine' => '/images/immagini/IMG_4155.jpg', 'alt' => 'Immagine1', 'titolo' => 'Titolo1', 'testo' => 'Testo1', 'data' => 'Data1'],
+    ['immagine' => '/images/immagini/IMG_4155a.jpg', 'alt' => 'Immagine2', 'titolo' => 'Titolo2', 'testo' => 'Testo2', 'data' => 'Data2'],
+    ['immagine' => '/images/immagini/IMG_4155.jpg', 'alt' => 'Immagine3', 'titolo' => 'Titolo3', 'testo' => 'Testo3', 'data' => 'Data3'],
+    ['immagine' => '/images/immagini/IMG_4155a.jpg', 'alt' => 'Immagine4', 'titolo' => 'Titolo4', 'testo' => 'Testo4', 'data' => 'Data4']
+];*/
 
 define('NEWS', $news);
 
+$sth = $pdo->prepare("SELECT * FROM articles");
+$sth->execute();
 
-$articles = [
-    ['immagine' => '/images/immagini/IMG_4175.jpg', 'alt' => 'Immagine1', 'titolo' => 'Titolo 1', 'testo' => 'Testo 1 eccetera'],
-    ['immagine' => '/images/immagini/IMG_4175.jpg', 'alt' => 'Immagine2', 'titolo' => 'Titolo 2', 'testo' => 'Testo 2 eccetera'],
-    ['immagine' => '/images/immagini/IMG_4175.jpg', 'alt' => 'Immagine3', 'titolo' => 'Titolo 3', 'testo' => 'Testo 3 eccetera'],
-    ['immagine' => '/images/immagini/IMG_4175.jpg', 'alt' => 'Immagine4', 'titolo' => 'Titolo 4', 'testo' => 'Testo 4 eccetera'],
-];
+$articles = $sth->fetchAll(\PDO::FETCH_ASSOC);
+
+/*$articles = [
+    ['immagine' => '/images/immagini/IMG_4175.jpg', 'alt' => 'Immagine1', 'titolo' => 'Titolo1', 'testo' => 'Testo1'],
+    ['immagine' => '/images/immagini/IMG_4175.jpg', 'alt' => 'Immagine2', 'titolo' => 'Titolo2', 'testo' => 'Testo2'],
+    ['immagine' => '/images/immagini/IMG_4175.jpg', 'alt' => 'Immagine3', 'titolo' => 'Titolo3', 'testo' => 'Testo3'],
+    ['immagine' => '/images/immagini/IMG_4175.jpg', 'alt' => 'Immagine4', 'titolo' => 'Titolo4', 'testo' => 'Testo4'],
+];*/
 
 define('ARTICLES', $articles);
