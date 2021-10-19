9fa61a6fb229bf12d8fca9f2d68b3f23c1e58030
Danixx91 <daniele_1991@hotmail.com>
2021-10-19 16:23:58 +0200

    blablublibla

--- partials/_db_close_connection.php
+++
@@ -1,2 +0,0 @@
-<?php
-$pdo = null;
--- prova.php
+++
@@ -1,7 +0,0 @@
-<?php require('partials/_header_new.php') ?>
-
-<div>fdsdfsdsfds</div>
-
-<?php require('partials/_carousel.php') ?>
-
-<?php require('partials/_footer.php') ?>
