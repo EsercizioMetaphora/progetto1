51a1865fc3972a544436381d4687fff1c12b4ce9
Cosimo <cosimo.matassini@libero.it>
2021-10-25 18:52:44 +0200

    nuovocommit
    commit1

--- partials/head.php
+++ partials/head.php
@@ -2,16 +2,17 @@
     <link rel="manifest" href="/manifest.json">
     <link rel="stylesheet" href="/styles/style.css">
 
-
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="description" content="Prova di metaphora con Qaulcosa di Seo"/>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
-    <title>ProvaGit</title>
+    <title>
+        <?php echo __('title') ?>
+    </title>
     <meta name="theme-color" content="#ff99cc">
     <link rel="apple-touch-icon" href="/images/icons/icon-512x512.png">
     <meta name="msapplication-TileColor" content="#ff99cc">
