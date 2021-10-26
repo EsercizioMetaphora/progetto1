33b309e79faf1e0d0f27bce2987a03af0c1fd6bc
Cosimo <cosimo.matassini@libero.it>
2021-10-26 15:49:12 +0200

    nuovo editarticle
    editarticle brian

--- edit_article.php
+++ edit_article.php
@@ -1,12 +1,19 @@
+<<<<<<< Updated upstream
 <?php require('partials/_header_new.php') ?>
 
 <?php
 /* article check*/
+=======
+<?php require 'partials/_header_new.php' ?>
+
+<?php
+>>>>>>> Stashed changes
 if (!isset($article_detail)) {
     http_response_code(404);
     require('partials/404.php');
     die();
 }
+<<<<<<< Updated upstream
 /* _POST check & field update */
 if (isset($_POST['id'])) {
     $article_id = $_POST['id'];
@@ -93,6 +100,60 @@ if (isset($_POST['id'])) {
             <div>
                 <button type="submit" class="my-3 btn btn-primary">Salva</button>
             </div>
+=======
+$errors = [];
+
+if (isset($_POST['id'])) {
+    $name = $_POST['name'];
+    $img = $_POST['img'];
+    $ord = $_POST['ord'];
+    $price = $_POST['price'];
+    $category = $_POST['category'];
+
+    $article_id = $_POST['id'];
+    if (strlen($_POST['name']) > 2) {
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    } else {
+        $errors['name']= 'Il titolo  deve essere più lungo di due caratteri!';
+
+    }
+   
+
+    if(count ($errors)===0){
+
+        $sth = $pdo->prepare("UPDATE article SET `name`= '$name', `img`= '$img',
+        `ord`= '$ord', `price`= '$price', `category`= '$category'  
+        WHERE id = $article_id");
+        $sth->execute();
+
+    } else{
+        var_dump($errors);
+    }
+    $sth = $pdo->prepare("SELECT * FROM article WHERE id = $article_id");
+    $sth->execute();
+
+    $article_detail = $sth->fetch(\PDO::FETCH_ASSOC);
+} else{
+    $name = $article_detail['name'];
+    $img = $article_detail['img'];
+    $ord = $article_detail['ord'];
+    $price = $article_detail['price'];
+    $category = $article_detail['category'];
+}
+?>
+<main>
+    <?php   ($article_detail) ?>
+    <form class="form-row" action="" method="POST">
+        <div class="form-group col-md-6">
+        <input class="form-control" type="hidden" name="id" value="<?php echo $article_detail['id']?>"><br>
+        <input class="form-control" type="text" name="name"  value="<?php echo $name?>"><br>
+        <input class="form-control" type="text" name="img" value="<?php echo $img?>" ><br>
+        <input class="form-control" type="text" name="ord" value="<?php echo $ord?>" ><br>
+        <input class="form-control" type="text" name="price" value="<?php echo $price?>" ><br>
+        <input class="form-control" type="text" name="category" value="<?php echo $category?>"><br>
+        <button class="btn btn-outline-primary d-flex justify-content-center" type="submit">Salva</button>
+        </div>
+>>>>>>> Stashed changes
     </form>
     <!--  PROVA BOTTONI PER COSARE LE COSE  -->
     <div class="d-flex justify-content-center">
