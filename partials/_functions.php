bb3f82e51c973af17a9b3cefe466e89496672b28
Cosimo <cosimo.matassini@libero.it>
2021-11-04 15:29:57 +0100

    aggiornamento pending

--- partials/_functions.php
+++ partials/_functions.php
@@ -29,3 +29,17 @@ function getArticleFoto($article)
     }
     return $array_return;
 }
+
+function __($str, $lang = 'it')
+{
+    global $pdo;
+    $sth = $pdo->prepare("SELECT * FROM `translation` WHERE `source` = '$str' AND `language` = '$lang' LIMIT 1");
+    $sth->execute();
+    $traduzione = $sth->fetch(\PDO::FETCH_ASSOC);
+    if (is_array($traduzione)) {
+        return $traduzione['target'];
+    } else {
+        return $str;
+    }
+}
+
