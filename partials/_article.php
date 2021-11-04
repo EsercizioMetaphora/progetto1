7b4e41dfceb473b30c51f1a2cdd150accfe206da
dhhdff <dhh_dff@yahoo.it>
2021-11-04 16:17:23 +0100

    Update _article.php

--- partials/_article.php
+++ partials/_article.php
@@ -7,10 +7,12 @@
                         <h2 class="title"><?php echo __('Home.Ultimi_Post') ?></h2>
                     </div>
                     <div class="search-box">
-                        <div class="input-search-box" data-aos="fade-up">
-                            <input class="input" name="q" value="<?php echo $get_q ?>" type="text" placeholder="Search here">
-                            <button class="submit-button"><i class="icofont-search-1"></i></button>
-                        </div>
+                        <form action="" method="GET">
+                            <div class="input-search-box" data-aos="fade-up">
+                                <input class="input" name="q" value="<?php echo $get_q ?>" type="text" placeholder="Search here">
+                                <button class="submit-button"><i class="icofont-search-1"></i></button>
+                            </div>
+                        </form>
                     </div>
                 </div>
 
@@ -18,29 +20,29 @@
         </div>
         <div class="row">
             <div class="col-lg-12">
-            <?php foreach(ARTICLES as $art_element):?>
-                <?php $autor = getAutor($art_element['autor_id'])?>
-                <!-- Single Recent Article Item Start -->
-                <div class="single-recent-article-item" data-aos="fade-up">
-                    <a href="#!" class="recent-article-thum">
-                        <img src="<?php echo $art_element['img']?>" alt="" />
-                    </a>
-                    <div class="recent-article-content">
-                        <div class="recent-article-post-author">
-                        <?php echo __('Home.By') ?> <a href="#"><?php echo $autor['nome']." ".$autor['cognome']?></a>
-                        </div>
-                        <h3 class="title"><a href="blog-details.html"><?php echo $art_element['titolo']?></a>
-                        </h3>
-                        <p class="dec mt-3"><?php echo $art_element['testo']?></p>
-                        <div class="recent-article-post-meta">
-                            <span class="post-date">
-                                <a href="#"><?php echo $art_element['art_date']?></a>
-                            </span>
-                            <span><?php echo $art_element['read_time']?></span>
+                <?php foreach (ARTICLES as $art_element) : ?>
+                    <?php $autor = getAutor($art_element['autor_id']) ?>
+                    <!-- Single Recent Article Item Start -->
+                    <div class="single-recent-article-item" data-aos="fade-up">
+                        <a href="#!" class="recent-article-thum">
+                            <img src="<?php echo $art_element['img'] ?>" alt="" />
+                        </a>
+                        <div class="recent-article-content">
+                            <div class="recent-article-post-author">
+                                <?php echo __('Home.By') ?> <a href="#"><?php echo $autor['nome'] . " " . $autor['cognome'] ?></a>
+                            </div>
+                            <h3 class="title"><a href="blog-details.html"><?php echo $art_element['titolo'] ?></a>
+                            </h3>
+                            <p class="dec mt-3"><?php echo $art_element['testo'] ?></p>
+                            <div class="recent-article-post-meta">
+                                <span class="post-date">
+                                    <a href="#"><?php echo $art_element['art_date'] ?></a>
+                                </span>
+                                <span><?php echo $art_element['read_time'] ?></span>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <!-- Single recent Article Item End -->
+                    <!-- Single recent Article Item End -->
                 <?php endforeach ?>
             </div>
         </div>
