db4abfbd16bab1698d2105b13afd1646a942fc79
Cosimo <cosimo.matassini@libero.it>
2021-11-09 20:36:44 +0100

    prova con db
    aggiunto database

--- partials/_head.php
+++ partials/_head.php
@@ -2,7 +2,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="./assets/img/favicon.png">
-  <title>Palestra Matassini</title>
+  <title>PalestraMatassini</title>
   <link rel="stylesheet" href="./assets/css/plugins.css">
   <link rel="stylesheet" href="./assets/css/style.css">
   <link rel="stylesheet" href="./assets/css/colors/orange.css">
