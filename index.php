5ecf56927a8934e4530ea95a5403763805511cf7
dhhdff <daniele_1991@hotmail.com>
2021-12-13 09:46:15 +0100

    primo commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php require('partials/db_connection.php') ?>
+<?php require('partials/website_data.php') ?>
+<?php require('partials/functions.php') ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require('partials/head.php') ?>
+
+<body>
+
+  <?php require('partials/header.php') ?>
+  <?php require('partials/story.php') ?>
+  <?php require('partials/pizze.php') ?>
+  <?php require('partials/trainers.php') ?>
+  <?php require('partials/abbonamenti.php') ?>
+  <?php require('partials/footer.php') ?>
+  <?php require('partials/script.php') ?>
+
+</body>
+
+</html>
